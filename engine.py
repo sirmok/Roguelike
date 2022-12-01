ca4f9ec29c732207200009b76b749ce1fa80d4d8
Frans <f.a.e.schooltink@student.utwente.nl>
2022-12-01 12:29:53 +0100

    commit changes

---
+++ engine.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from tcod.context import Context
+from tcod.console import Console
+from tcod.map import compute_fov
+
+from input_handlers import EventHandler
+
+if TYPE_CHECKING:
+    from entity import Entity
+    from game_map import GameMap
+
+class Engine:
+    game_map: GameMap
+
+    def __init__(self, player: Entity):
+        self.event_handler = EventHandler(self)
+        self.player = player
+
+    def handle_enemy_turns(self) -> None:
+        for entity in self.game_map.entities - {self.player}:
+            print("lol")
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # IF a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+
+    def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+
+        context.present(console)
+
+        console.clear()
